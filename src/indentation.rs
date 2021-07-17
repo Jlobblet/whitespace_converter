3cee8cde87643e41ab0097be8192db6b79f8da59
Jlobblet <jlobblet-github@gmx.co.uk>
2021-07-17 11:07:54 +0100

    Fix the bug the tests found

--- src/indentation.rs
+++ src/indentation.rs
@@ -27,26 +27,22 @@ impl Indentation {
     pub(crate) fn make_transformation(&self, buf: String) -> String {
         return if let Some(index) = match self {
             Indentation::Tabs(_) => buf.find(|c| c != ' '),
-            Indentation::Spaces(_) => buf.find(|c| c != '\t')
+            Indentation::Spaces(_) => buf.find(|c| c != '\t'),
         } {
             if index == 0 {
                 return buf;
             }
-            let mut new = String::with_capacity(buf.capacity());
             let (char, width) = match self {
-                Indentation::Tabs(u) => {
-                    ('\t', index / u)
-                }
-                Indentation::Spaces(u) => {
-                    (' ', index * u)
-                }
+                Indentation::Tabs(u) => ('\t', index / u),
+                Indentation::Spaces(u) => (' ', index * u),
             };
-            new.push_str(format!("{:width$}", char, width = width).as_str());
+            let mut new = std::iter::repeat(char).take(width).collect::<String>();
+            new.reserve_exact(buf.capacity());
             new.push_str(&buf[index..]);
             new
         } else {
             buf
-        }
+        };
     }
 }
 
